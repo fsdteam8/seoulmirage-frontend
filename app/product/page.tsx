29e5c33934ccf1d420c7eed8d1a0325720028e0c
ILhankhondaker <ilhankhondaker@gmail.com>
2025-06-01 10:30:27 +0600

    fix up

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,13 +1,12 @@
+import AllProduct from "@/components/Product/AllProduct";
 
-
-import ProductGrid from "@/components/Product/ProductGrid";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8">Beauty Products</h1>
-        <ProductGrid />
+        <h1 className="text-3xl font-bold text-center mb-8">All  Products</h1>
+       <AllProduct/>
       </div>
     </div>
   )
