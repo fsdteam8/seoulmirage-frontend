bf2d7ae658313c106ac792b70c44f0a0a98da017
ILhankhondaker <ilhankhondaker@gmail.com>
2025-06-01 12:06:58 +0600

    add font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={raleway.className}>
       <body className={`${raleway.variable} antialiased`}>
        <Navbar/>
         {children}
